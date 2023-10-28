83644d20c1fffd042f32cc20729bdd7ecfbc87e7
AniketPatil20 <95218817+AniketPatil20@users.noreply.github.com>
2023-10-29 00:16:08 +0530

    Done

---
+++ temp/8e0xdgr.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+cout<<"Hiiiiiiiiiiiiiiiiiiii";
+  // prints sum 
+  //cout << first_number << " + " <<  second_number << " = " << sum; 
+
+  return 0;
+}
---
+++ temp/en3xdrr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+cout<<"Hiiiiiiii";
+  // prints sum 
+  int a=10;
+  cout<<a;
+  //cout << first_number << " + " <<  second_number << " = " << sum; 
+
+  return 0;
+}
